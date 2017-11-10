552e2106729b4ec89906304ed202d0d15264f654
poohpaintii <poohpaintii@gmail.com>
2017-11-10 11:12:04 +0700

    login

--- Lab10_WindowsFormProperties/Lab10_WindowsFormProperties/Form1.Designer.cs
+++ Lab10_WindowsFormProperties/Lab10_WindowsFormProperties/Form1.Designer.cs
@@ -110,7 +110,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form Properties";
+            this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
